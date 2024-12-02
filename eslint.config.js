5f0744e7c21ccb877a23a0118822a06415ef3b0b
Matt Mundell <matt@mundell.me>
2024-12-03 00:24:53 +0200

    u @cookshack/eslint-config

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-import * as EslintConfig from '@cookshack/eslint-config'
-
-export
-default [ { ignores: [ 'TAGS.mjs', 'json.mjs' ] },
-          { files: [ '*.js', '*.mjs', 'src/*.js' ],
-            languageOptions: EslintConfig.languageOptions,
-            plugins: EslintConfig.plugins,
-            rules: EslintConfig.rules } ]
